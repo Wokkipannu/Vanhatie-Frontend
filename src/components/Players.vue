465d3feb37499b88a807c2d7b571d24fc8e509c2
Wokkipannu <iiro@kooditaikuri.fi>
2019-08-18 14:58:05 +0300

    Races and classes

--- src/components/Players.vue
+++ src/components/Players.vue
@@ -19,12 +19,25 @@
         <b-spinner class="align-middle"></b-spinner>
         <strong class="ml-2">Loading...</strong>
       </div>
+
       <template slot="[name]" slot-scope="player" v-if="isLogged">
         <b-form-input type="text" placeholder="Pelaajan nimi" v-model="player.item.name"></b-form-input>
       </template>
+
       <template slot="[dkp]" slot-scope="player" v-if="isLogged">
         <b-form-input type="number" placeholder="DKP" v-model="player.item.dkp"></b-form-input>
       </template>
+
+      <template slot="[race]" slot-scope="player">
+        <b-form-select v-model="player.item.race" :options="races" v-if="isLogged"></b-form-select>
+        <div v-if="!isLogged"><b-img :src="getRaceIcon(player.item.race)"/> {{ player.item.race }}</div>
+      </template>
+
+      <template slot="[class]" slot-scope="player">
+        <b-form-select v-model="player.item.class" :options="classes" v-if="isLogged"></b-form-select>
+        <div v-if="!isLogged"><b-img :src="getClassIcon(player.item.class)"/> {{ player.item.class }}</div>
+      </template>
+
       <template slot="[actions]" slot-scope="player">
         <b-input-group>
           <b-form-input type="text" placeholder="Syy" v-model="player.item.reason"></b-form-input>
@@ -55,6 +68,14 @@ export default {
           label: 'Nimi',
           sortable: true
         },
+        race: {
+          label: 'Race',
+          sortable: true
+        },
+        class: {
+          label: 'Class',
+          sortable: true
+        },
         dkp: {
           label: 'DKP',
           sortable: true
@@ -65,7 +86,9 @@ export default {
       options: [],
       search: '',
       error: null,
-      newUser: ''
+      newUser: '',
+      races: ['N/A', 'Orc', 'Tauren', 'Troll', 'Undead'],
+      classes: ['N/A', 'Druid', 'Hunter', 'Mage', 'Priest', 'Rogue', 'Shaman', 'Warlock', 'Warrior']
     }
   },
   sockets: {
@@ -74,7 +97,10 @@ export default {
       this.playersCopy.push(JSON.parse(JSON.stringify(player)));
     },
     updatePlayer(player) {
-      this.players.find(p => p._id === player._id).dkp = player.dkp;
+      let p = this.players.find(p => p._id === player._id);
+      Object.keys(player).forEach(key => {
+        p[key] = player[key];
+      });
     }
   },
   beforeMount() {
@@ -95,24 +121,31 @@ export default {
     update(player) {
       this.loading = true;
 
-      let copyPlayer = this.playersCopy.find(p => p._id === player._id);
+      this.$http.post(`${process.env.VUE_APP_API_ENDPOINT}/api/v1/users/token`, { token: localStorage.token }).then(res => {
+        let copyPlayer = this.playersCopy.find(p => p._id === player._id);
 
-      let content = [];
-      if (copyPlayer.name !== player.name) content.push(`> Pelaajan **${copyPlayer.name}** nimi muuetttiin, uusi nimi **${player.name}**`);
-      if (copyPlayer.dkp !== player.dkp) content.push(`> Pelaajan **${player.name}** DKP muutettiin **${player.dkp - copyPlayer.dkp}** (Nyt ${player.dkp})`);
-      if (player.reason) content.push(`> **Syy:** ${player.reason}`);
-      
-      const payload = {
-        content: content.join('\n')
-      };
+        let content = [];
+        content.push(`> **${res.data.data.data.username}** teki muutoksia pelaajaan **${copyPlayer.name}**`);
+        if (copyPlayer.name !== player.name) content.push(`> Uusi nimi **${player.name}**`);
+        if (copyPlayer.dkp !== player.dkp) content.push(`> DKP muutettiin **${player.dkp - copyPlayer.dkp}** (Nyt ${player.dkp})`);
+        if (copyPlayer.race !== player.race) content.push(`> Uusi race **${player.race}**`);
+        if (copyPlayer.class !== player.class) content.push(`> Uusi class **${player.class}**`);
+        if (player.reason) content.push(`> **Syy:** ${player.reason}`);
+        
+        const payload = {
+          content: content.join('\n')
+        };
 
-      delete player.reason;
+        delete player.reason;
 
-      this.$http.put(`${process.env.VUE_APP_API_ENDPOINT}/api/v1/players/${player._id}?token=${localStorage.token}`, player).then(() => {
-        this.$http.post(`${process.env.VUE_APP_WEBHOOK}`, JSON.stringify(payload));
-        copyPlayer.dkp = player.dkp;
-        copyPlayer.name = player.name;
-        this.loading = false;
+        this.$http.put(`${process.env.VUE_APP_API_ENDPOINT}/api/v1/players/${player._id}?token=${localStorage.token}`, player).then(() => {
+          this.$http.post(`${process.env.VUE_APP_WEBHOOK}`, JSON.stringify(payload));
+          Object.keys(player).forEach(key => copyPlayer[key] = player[key]);
+          this.loading = false;
+        })
+        .catch(error => {
+          this.error = error.response.data.message;
+        });
       })
       .catch(error => {
         this.error = error.response.data.message;
@@ -132,6 +165,16 @@ export default {
       else {
         this.error = 'Uuden käyttäjän nimi tulee olla vähintään ';
       }
+    },
+
+    getRaceIcon(val) {
+      if (this.races.includes(val) && val !== 'N/A') return require(`../assets/race_${val.toLowerCase()}.jpg`);
+      else return require('../assets/na.png');
+    },
+
+    getClassIcon(val) {
+      if (this.classes.includes(val) && val !== 'N/A') return require(`../assets/class_${val.toLowerCase()}.jpg`);
+      else return require('../assets/na.png');
     }
   },
   computed: {
