967c55aedd13333297d80de625aa1d5e912dddf0
Wokki <iiro@kooditaikuri.fi>
2020-01-26 01:56:12 +0200

    Added footer

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,32 @@
 <template>
   <div id="app">
-    <b-navbar type="dark">
-      <b-container>
-        <b-navbar-nav>
-          <router-link to="/" tag="b-nav-item">Pelaajat</router-link>
-          <router-link to="/login" tag="b-nav-item" v-if="!isLogged">Kirjaudu sisään</router-link>
-          <router-link to="/users" tag="b-nav-item" v-if="isLogged">Käyttäjät</router-link>
-          <b-nav-item v-if="isLogged" v-on:click="logout">Kirjaudu ulos</b-nav-item>
-        </b-navbar-nav>
-      </b-container>
-    </b-navbar>
+    <main>
+      <b-navbar type="dark">
+        <b-container>
+          <b-navbar-nav>
+            <router-link to="/" tag="b-nav-item">Pelaajat</router-link>
+            <router-link to="/login" tag="b-nav-item" v-if="!isLogged">Kirjaudu sisään</router-link>
+            <router-link to="/users" tag="b-nav-item" v-if="isLogged">Käyttäjät</router-link>
+            <b-nav-item v-if="isLogged" v-on:click="logout">Kirjaudu ulos</b-nav-item>
+          </b-navbar-nav>
+        </b-container>
+      </b-navbar>
 
-    <router-view/>
+      <router-view/>
+    </main>
+
+    <Footer />
   </div>
 </template>
 
 <script>
+import Footer from '@/components/Footer.vue';
+
 export default {
   name: 'app',
+  components: {
+    Footer
+  },
   methods: {
     logout() {
       localStorage.removeItem('token');
---
+++ src/components/Footer.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <footer class="footer">
+      <b-container>
+        <p class="mb-0"><b-icon icon="cone-striped" variant="warning"></b-icon> Vanha tie DKP app by Wokki</p>
+        <p class="mt-0"><a href="https://github.com/Wokkipannu/Vanhatie-Frontend" target="_blank">Github</a></p>
+      </b-container>
+    </footer>
+  </div>
+</template>
+
+<style>
+html, body {
+  width: 100%;
+  height: 100%;
+}
+
+#app {
+  min-height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: stretch;
+}
+
+main {
+  flex-grow: 1;
+}
+
+main, .footer {
+  flex-shrink: 0;
+}
+</style>
